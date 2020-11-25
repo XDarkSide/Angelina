77b2bab339e1c42af393ee6e2f02b4b99d2231a4
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2020-11-25 22:53:42 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -130,7 +130,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("waked up😏😏😏")
+        update.effective_message.reply_text("Heya:), I am Angelina!\nPM me if you have any questions on how to use me!")
 
 
 # for test purposes
