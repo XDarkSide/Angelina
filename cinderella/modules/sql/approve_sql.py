10e1960691214d3600ade4bca29a097dcc9d5ad1
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2020-12-21 20:08:15 +0530

    Update approve_sql.py

--- cinderella/modules/sql/approve_sql.py
+++ cinderella/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from cinderella.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
