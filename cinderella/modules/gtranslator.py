be1a90724946d26211c9754efce57c19ca1b6abb
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2020-12-22 23:45:10 +0530

    Update gtranslator.py

--- cinderella/modules/gtranslator.py
+++ cinderella/modules/gtranslator.py
@@ -108,9 +108,7 @@ def totranslate(bot: Bot, update: Update):
     else:
         return
 
-__help__ = """
-- /tr (language code) as reply to a long message.
-"""
+
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler("tr", totranslate)
 
