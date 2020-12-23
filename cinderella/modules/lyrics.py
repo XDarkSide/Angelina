5b6c00719105c5d084b9f7bd4d2bd81a989e7818
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2020-12-23 21:52:21 +0530

    Update lyrics.py

--- cinderella/modules/lyrics.py
+++ cinderella/modules/lyrics.py
@@ -38,9 +38,14 @@ def lyrics(bot: Bot, update: Update, args):
 __help__ = """
 Want to get the lyrics of your favorite songs straight from the app? This module is perfect for that!
 
-*Available commands:*
- - /lyrics <song>: returns the lyrics of that song.
- You can either enter just the song name or both the artist and song name.
+*Command:*
+ - /lyrics `<song>`: Returns the lyrics of that song.
+ 
+• You can either enter just the song name or both the artist and song name.
+
+*Example:*
+- To get lyrics of Despacito
+-> `/lyrics despacito`
 """
 
 __mod_name__ = "SONG LYRICS"
