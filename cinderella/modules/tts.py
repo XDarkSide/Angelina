870e848cf1b23b8f7e87814b889383c85dfed8ed
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2020-12-25 15:16:13 +0530

    Update tts.py

--- cinderella/modules/tts.py
+++ cinderella/modules/tts.py
@@ -17,25 +17,25 @@ from cinderella.__main__ import STATS
 from cinderella.modules.disable import DisableAbleCommandHandler
 from cinderella.modules.helper_funcs.extraction import extract_user
 
-def tts(bot: Bot, update: Update, args):
+def speak(bot: Bot, update: Update, args):
     current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
     filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
     reply = " ".join(args)
     update.message.chat.send_action(ChatAction.RECORD_AUDIO)
     lang="ml"
-    tts = gTTS(reply, lang)
-    tts.save("k.mp3")
+    speak = gTTS(reply, lang)
+    speak.save("k.mp3")
     with open("k.mp3", "rb") as f:
         linelist = list(f)
         linecount = len(linelist)
     if linecount == 1:
         update.message.chat.send_action(ChatAction.RECORD_AUDIO)
         lang = "en"
-        tts = gTTS(reply, lang)
-        tts.save("k.mp3")
+        speak = gTTS(reply, lang)
+        speak.save("k.mp3")
     with open("k.mp3", "rb") as speech:
         update.message.reply_voice(speech, quote=False)
 
 __mod_name__ = "Text To Speech"
 
-dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
+dispatcher.add_handler(CommandHandler('speak', speak, pass_args=True))
