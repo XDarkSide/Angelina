b7b8cf62f61374197bfe2424213c2cea377939c6
AnonyIndian <73958752+anonyindian@users.noreply.github.com>
2020-12-27 21:59:14 +0530

    Update __init__.py

--- Angelina/__init__.py
+++ Angelina/__init__.py
@@ -84,7 +84,7 @@ if ENV:
     SW_API = os.environ.get('SW_API', None)
     
 else:
-    from cinderella.config import Development as Config
+    from Angelina.config import Development as Config
     TOKEN = Config.API_KEY
 
     try:
@@ -171,7 +171,7 @@ else:
         spam_watch = None
         
 # Load at end to ensure all prev variables have been set
-from cinderella.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
+from Angelina.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
